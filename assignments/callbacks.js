02206e95c162051242762534071f0f2bde54ddf2
Unknown <bkoehler85282@gmail.com>
2019-10-03 20:56:12 -0700

    used arrow functions when needed.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -42,7 +42,7 @@ function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
 }
-getLength(items, function(arrLength) {
+getLength(items, arrLength => {
   console.log(arrLength);
 });
 
@@ -50,7 +50,7 @@ function last(arr, cb) {
   // last passes the last item of the array into the callback.\
   return cb(arr[arr.length - 1]);
 }
-last(items, function(lastItem) {
+last(items, lastItem => {
   console.log(lastItem);
 });
 
@@ -58,7 +58,7 @@ function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x, y);
 }
-sumNums(2, 4, function(x, y) {
+sumNums(2, 4, (x, y) => {
   console.log(x + y);
 });
 
@@ -66,7 +66,7 @@ function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
   return cb(x, y);
 }
-multiplyNums(3, 7, function(x, y) {
+multiplyNums(3, 7, (x, y) => {
   console.log(x * y);
 });
 
@@ -80,7 +80,7 @@ function contains(item, list, cb) {
   }
 }
 
-contains('Pencil', items, function(result) {
+contains('Pencil', items, result => {
   console.log(result);
 });
 
