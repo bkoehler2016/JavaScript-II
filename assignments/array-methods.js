5b180b6be471f8b262a4349f36a319496ce91356
bkoehler2016 <bkoehler85282@gmail.com>
2019-10-06 16:10:03 -0700

    added currency indicater when money was involved

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -488,7 +488,7 @@ ticketPriceTotal = runners.reduce(
   0
 );
 
-console.log(ticketPriceTotal);
+console.log(`The total amount of the donations received is $${ticketPriceTotal}`);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
